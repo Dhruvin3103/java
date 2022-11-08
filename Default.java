76a45b36fd0c04f9d5cec80ccb53bf955c2294f1
Dhruvin3103 <dcchawda2003@gmail.com>
2022-11-08 19:28:59 +0530

    Exp 2.4

---
+++ Default.java
@@ -0,0 +1,20 @@
+class Default {
+    public static int i;
+    public static double d;
+    public static short s;
+    public static long l;
+    public static float f;
+    public static char c;
+    public static boolean b;
+    public static byte bi;
+    public static void main(String[] args) {
+    System.out.println("Default Value of Integer: " + i);
+    System.out.println("Default Value of Double: " + d);
+    System.out.println("Default Value of Float: " + f);
+    System.out.println("Default Value of Char: " + c);
+    System.out.println("Default Value of Boolean: " + b);
+    System.out.println("Default Value of Long: " + l);
+    System.out.println("Default Value of Short: " + s);
+    System.out.println("Default Value of Byte: " + bi);
+    }
+    }
