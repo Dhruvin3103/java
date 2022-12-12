568a077455fbe64df2e56fccd22813c4d4d39c5e
Dhruvin3103 <dcchawda2003@gmail.com>
2022-12-12 19:26:19 +0530

    Commit

--- MainthreadDemo.java
+++ MainthreadDemo.java
@@ -2,9 +2,37 @@ class MainthreadDemo
 {
 public static void main(String args[])
 {
-Thread t = Thread.currentThread();
-System.out.println("Cuurent thread : "+t);
-t.setName("T name");
-System.out.println("Renamed : "+t);
+Even e = new Even();
+
+e.start();
+Odd o = new Odd();
+o.start();
 }
 }
+
+
+class Odd extends Thread
+{
+public void run()
+{
+for(int i=0;i<20;i++)
+{
+if(i%2!=0){System.out.println("Odd "+i);}
+try
+{
+Thread.sleep(100);
+}catch(Exception e){System.out.println(e);}
+}}}
+
+class Even extends Thread
+{
+public void run()
+{
+for(int i=0;i<20;i++)
+{
+if(i%2==0){System.out.println("Even "+i);}
+try
+{
+Thread.sleep(200);
+}catch(Exception e){System.out.println(e);}
+}}}
