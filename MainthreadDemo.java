4647757fcac30b20417514b8dcf6c8d5c1b59d36
Dhruvin3103 <dcchawda2003@gmail.com>
2022-12-12 19:43:59 +0530

    Another ex of interface

--- MainthreadDemo.java
+++ MainthreadDemo.java
@@ -2,14 +2,27 @@ class MainthreadDemo
 {
 public static void main(String args[])
 {
-Even e = new Even();
-
-e.start();
+Prime e = new Prime();
+Nat n = new Nat();
 Odd o = new Odd();
 o.start();
+e.start();
+n.start();
 }
 }
 
+class Nat extends Thread
+{
+public void run()
+{
+for(int i=0;i<20;i++)
+{
+{System.out.println("Nat "+i);}
+try
+{
+Thread.sleep(100);
+}catch(Exception e){System.out.println(e);}
+}}}
 
 class Odd extends Thread
 {
@@ -20,19 +33,22 @@ for(int i=0;i<20;i++)
 if(i%2!=0){System.out.println("Odd "+i);}
 try
 {
-Thread.sleep(100);
+Thread.sleep(200);
 }catch(Exception e){System.out.println(e);}
 }}}
 
-class Even extends Thread
+class Prime extends Thread
 {
+int j;
 public void run()
 {
-for(int i=0;i<20;i++)
+for(int i=1;i<20;i++)
 {
-if(i%2==0){System.out.println("Even "+i);}
+for(j=2;j<i;j++)
+{if(i%j==0)break;}
+if(i==j){System.out.println("Prime "+i);}
 try
 {
-Thread.sleep(200);
+Thread.sleep(300);
 }catch(Exception e){System.out.println(e);}
 }}}
